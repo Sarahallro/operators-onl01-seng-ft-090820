3fa9e431ad2aa9ff0d7cb5ca435636b690d5155e
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-13 20:14:50 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,27 @@
 def unsafe?(speed)
-
+  if speed > 60
+    true
+  elsif speed < 40
+    true
+  else 
+    false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  if speed > 60
+    true
+  elsif speed < 40
+    true
+  else 
+    false
+  end
 end
 	
 
 
+ #(speed > 60) ? true : true
+#(speed > 60) ? true : false
+ # (speed < 40) ? true : false
